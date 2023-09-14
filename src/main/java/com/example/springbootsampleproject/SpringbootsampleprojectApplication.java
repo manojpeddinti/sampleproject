371eecc9e0c00e7eed7742a3a78f661ffcca3e40
PEDDINTI's <youremail@yourdomain.com>
2023-09-14 20:11:58 +0530

    springboot sample

---
+++ src/main/java/com/example/springbootsampleproject/SpringbootsampleprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootsampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsampleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsampleprojectApplication.class, args);
+		System.out.println("First Springboot program");
+	}
+
+}
---
+++ src/test/java/com/example/springbootsampleproject/SpringbootsampleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsampleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
